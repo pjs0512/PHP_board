6235dd66fcbcfd384527095c64052f5a1cd39c61
ParkJunSang <egt5001@naver.com>
2017-09-25 20:29:28 +0900

    원래대로

--- contentsView.php
+++ contentsView.php
@@ -91,12 +91,7 @@ $selectValue = $db->select($con,array("board","board_id",$_GET['board_id']));
                                     </tr>
                                 </table>
                                     <?php
-                            }
-                        }
-                                    $selectReply=$db->replySelectDESC($con,array($selectValue[0]['board_id']));
-                                    for($i=0;$i<count($selectReply);$i++){
-                                    if($selectReply[$i]['reply_subid']==0){}else{
-
+                            }else{
                                         $selectName=$db->replySelect($con,array($selectValue[0]['board_id'],$selectReply[$i]['reply_subid']));
                                         $name = $selectName[0]['user_name'];
                                 ?>
